75a57c986f1b380ab3ab8a85aa7225310ac82814
Joffrey Herard <joffrey.herard@etudiant.univ-reims.fr>
2016-12-05 15:46:15 +0100

    ChangeProfil
    A prevoir ajout des requette http pour actualiser les accepter les changements

--- TyrellServices/src/pages/changeprofile/changeprofile.ts
+++ TyrellServices/src/pages/changeprofile/changeprofile.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController,NavParams,AlertController } from 'ionic-angular';
 
-/*
-  Generated class for the Changeprofile page.
 
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-changeprofile',
   templateUrl: 'changeprofile.html'
