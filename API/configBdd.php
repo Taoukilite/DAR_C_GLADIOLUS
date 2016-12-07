61c5f4566a6d73037c729380bbd343fdc4cba3e8
Alexandre DERNIAME <kiralex1995@gmail.com>
2016-12-07 20:09:02 +0100

    api : recherche url boutiques depuis localisation

--- API/configBdd.php
+++ API/configBdd.php
@@ -1,5 +1,4 @@
 <?php
 require_once 'myPDO.include.php' ;
-
 header('Access-Control-Allow-Origin: *');
-myPDO::setConfiguration('mysql:host=localhost;dbname=tyrell;charset=utf8','tyrell','tyrell') ;
+myPDO::setConfiguration('mysql:host=localhost;dbname=TyrellReims;charset=utf8','tyrell','tyrell') ;
