c0508596b244134b51f78b793aa07bfbf6fb9986
Alexandre DERNIAME <kiralex1995@gmail.com>
2016-12-07 19:38:54 +0100

    api : recup infos client

--- API/configBdd.php
+++ API/configBdd.php
@@ -1,3 +1,5 @@
 <?php
 require_once 'myPDO.include.php' ;
+
+header('Access-Control-Allow-Origin: *');
 myPDO::setConfiguration('mysql:host=localhost;dbname=tyrell;charset=utf8','tyrell','tyrell') ;
